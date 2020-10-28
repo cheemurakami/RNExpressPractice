88d1c413da99f129d92a60854b557c5e4fbcf9b1
Chisato Murakami <kiwipom1031@gmail.com>
2020-10-28 13:26:53 -0700

    layout practice -boxes

--- App.js
+++ App.js
@@ -5,7 +5,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.box}>
+        <Text style={styles.boxText}>box</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.boxText}>box</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.boxText}>box</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +22,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "row",
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  box: {
+    width: 150,
+    height: 150,
+    backgroundColor: '#3B6CD4',
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 4,
+  },
+  boxText: {
+    color: 'white',
+    textAlign: 'center',
   },
 });
